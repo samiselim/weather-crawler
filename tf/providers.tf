9aa6ea07a4162319a4d41d33d73a32a4f30c4c92
sami.selim@vodafone.com <sami.selim@vodafone.com>
2025-04-05 00:29:20 +0200

    adding providers

--- tf/providers.tf
+++ tf/providers.tf
@@ -6,3 +6,22 @@ provider "helm" {
     config_path = "/etc/rancher/rke2/rke2.yaml"
   }
 }
+provider "kubernetes" {
+  config_path = "/etc/rancher/rke2/rke2.yaml"
+}
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = ">= 2.0.2"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.0.1"
+    }
+  }
+}
