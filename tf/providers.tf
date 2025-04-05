8845f8d12855cd21144bf2f7cbbee148df36e70e
sami.selim@vodafone.com <sami.selim@vodafone.com>
2025-04-05 04:09:33 +0200

    fix

--- tf/providers.tf
+++ tf/providers.tf
@@ -9,19 +9,19 @@ provider "helm" {
 provider "kubernetes" {
   config_path = "/etc/rancher/rke2/rke2.yaml"
 }
-terraform {
-  required_providers {
-    kubectl = {
-      source  = "alekc/kubectl"
-      version = ">= 2.0.2"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.5.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.0.1"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     kubectl = {
+#       source  = "alekc/kubectl"
+#       version = ">= 2.0.2"
+#     }
+#     helm = {
+#       source  = "hashicorp/helm"
+#       version = "2.5.0"
+#     }
+#     kubernetes = {
+#       source  = "hashicorp/kubernetes"
+#       version = "2.0.1"
+#     }
+#   }
+# }
